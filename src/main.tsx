8f11024beee2cf55c8dd5553f0b002012e1fd8cf
vision-al-desarrollo-chacchan <carbajalandre5829@gmail.com>
2026-08-10 11:24:32 -0500

    Mejorar tiempo real y manejo de errores

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
+import { NotificationProvider } from "./context/NotificationContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </ThemeProvider>
   </StrictMode>
 );
